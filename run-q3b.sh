a89f2eb9db9964ed684dde8bf6dc23585e70123c
alfie-ns <alfienurse@gmail.com>
2024-07-17 17:42:15 +0100

    update

--- run-q3b.sh
+++ run-q3b.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# ---------------- Run the program ----------------
+
+# 1. Clear previous run's output
+#--------------------------------
+
 cd q3/q3
-clang++ -std=c++17 q3b-mac.cpp -o image_processor -O3 -lm # Compile the program
+rm -f image_processor # Force remove the executable file
+cd q3-images/output_images # Change directory to output_images
+for i in *; do
+    rm -f $i # Force remove all files in output_images
+done
+cd ../../ # Change directory back to q3
+
+# 2. Compile program
+# -------------------
+
+clang++ -std=c++17 q3b-mac.cpp -o image_processor -O3 -lm 
+
+# 3. Run the program
+# -------------------
 ./image_processor q3-images/input_images q3-images/output_images # Run the program in the respective directorys, input_images and output_images

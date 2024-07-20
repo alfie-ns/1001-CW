53ca87333f6522115f953f5f197ae66f4fc42609
alfie-ns <alfienurse@gmail.com>
2024-07-20 20:51:10 +0100

    update

--- q3/q3/q3a.sh
+++ q3/q3/q3a.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 # Compile C++ program
-g++ -o q3 ../../q3/q3a.cpp -lm # -lm for the math library
+if g++ -o q3 ../../q3/q3a.cpp -lm; then
+    echo "q3a.cpp compiled successfully"
+else
+    echo "Error: q3a.cpp did not compile successfully"
+    exit 1 #failure
+fi
 
 # Create variables to hold the image locations. Use the full path name for each image.
 input_image=../../q3-images/input_images/a1.pgm
 blur_image=../../q3-images/output_images/blurred.pgm
 edge_image=../../q3-images/output_images/edge_detected.pgm
 
-# Run the program, passing the variables for the 3 image locations
-./q3 $input_image $blur_image $edge_image
+# Run program, passing the variables for the 3 image locations
+if ./q3 $input_image $blur_image $edge_image; then
+    echo "q3a.sh executed successfully"
+    exit 0 #success
+else
+    echo "Error: q3a.sh did not execute successfully"
+    exit 1 #failure
+fi
 
 # Compile bash script with: chmod +x bash-script.sh
 

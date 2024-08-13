a9b2f9d26046c140275ed35e8928cf600b833559
alfie-ns <alfienurse@gmail.com>
2024-08-13 09:05:30 +0100

    update

--- run/clear.bat
+++ run/clear.bat
@@ -1,6 +1,5 @@
 @echo off
 :: 'Q' flag: Quiet mode, suppresses confirmation prompt for faster deletion
-:: Only displays results of commands, not the commands themselves
 
 del /Q "../q3/q3/q3-images\output_images\*"
 del /Q "image_processor"

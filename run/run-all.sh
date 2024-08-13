e69eca0251b91cecbc21e3c6a023688585b839dd
alfie-ns <alfienurse@gmail.com>
2024-08-13 13:14:37 +0100

    update

--- run/run-all.sh
+++ run/run-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+
 echo -e '\nRunning q1...\n'
-bash q1.sh
-sleep 3
+if bash q1.sh; then sleep 3; fi
+
 echo -e '\nRunning q2...\n'
-bash q2.sh
-sleep 3
+if bash q2.sh; then sleep 4; fi
+
 echo -e '\nRunning q3a...\n'
-bash q3a.sh
-sleep 3
-echo -e 'Running q3b...\n'
-bash q3b.sh
-echo -e '\nall questions ran succesfully\n'
+if bash q3a.sh; then sleep 3; fi
+
+echo -e '\nRunning q3b...\n'
+if bash q3b.sh; then echo -e '\nall questions ran succesfully\n'; fi

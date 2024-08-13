39cf427743d18768e2d286ce466491746b6e9bea
alfie-ns <alfienurse@gmail.com>
2024-08-13 08:29:19 +0100

    update

--- run/q1.sh
+++ run/q1.sh
@@ -5,5 +5,5 @@ if clang++ -std=c++11 -O3 -march=armv8-a+simd q1-mac.cpp -o q1-mac-executable; t
     echo -e "\nCompiled q1-mac.cpp successfully\n"
 fi
 
-if ./q1-mac-executable; then sleep 2 && rm -f q1-mac-executable; 
+if ./q1-mac-executable; then sleep 1 && rm -f q1-mac-executable; 
 else echo -e "\nCould not run q1-mac-executable\m"; fi

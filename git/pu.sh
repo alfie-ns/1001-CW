f9835a90e79ee14e8edcc6b3a051a4fe2400bd23
alfie-ns <alfienurse@gmail.com>
2024-08-13 13:21:08 +0100

    update

--- git/pu.sh
+++ git/pu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+if ./clear-ds.sh; then sleep 0.1; else echo -e '\nfailed to clear .DS_Store files\n'; fi
 cd ../run
-if ./clear.sh; then sleep 1; else echo -e "\nfailed to clear output; exiting\n"; exit 1; fi
+if ./clear.sh; then sleep 1; else echo -e "\nfailed to clear output. Exiting...\n"; exit 1; fi
 # ---------------------------------------------------------------------------------------------------
 cd ..
 git add .

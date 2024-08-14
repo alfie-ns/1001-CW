6b187c09f07824e0cdce5aa6b18f1d841cf37634
alfie-ns <alfienurse@gmail.com>
2024-08-14 15:23:21 +0100

    update

--- git/pu.sh
+++ git/pu.sh
@@ -3,8 +3,6 @@ if ./clear-ds.sh; then :; else echo -e '\nfailed to clear .DS_Store files. Exiti
 cd ../run
 if ./clear.sh; then :; else echo -e "\nfailed to clear output. Exiting...\n"; exit 1; fi
 # ---------------------------------------------------------------------------------------------------
-cd ..
-git add .
-git commit -m 'update'
-git push origin main
+cd .. && git add .
+if git commit -m 'update'; then git pull origin main; else echo -e "\nfailed to commit. Exiting...\n"; exit 1; fi
 echo -e "\ngit pushed to main\n"

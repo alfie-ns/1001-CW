def18c57ec50a06d5ce19cb06ece4eaadfc37bd6
alfie-ns <alfienurse@gmail.com>
2024-08-15 19:36:38 +0100

    update

--- run/run-all.sh
+++ run/run-all.sh
@@ -7,7 +7,7 @@ if bash q3a.sh; then echo -e '\nRunning q3a...\n'; fi
 # ---------------------------------------------------
 read -p "Do you want to proceed to q3b? This will clear the screen (y/n): " answer
 if [[ $answer == "y" ]]; then bash q3b.sh; fi
-# if answer is anything else, skip q3b
+# if answer's anything else, will skip q3b
 : <<'END'
 
 If all the listed scripts run successfully,

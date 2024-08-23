fce4cca4200653f584ee04c5be482da9cb5c98a0
alfie-ns <alfienurse@gmail.com>
2024-08-23 13:38:16 +0100

    update

--- run/clear.sh
+++ run/clear.sh
@@ -3,4 +3,4 @@ rm -f ../q3/q3/q3-images/output_images/*
 rm -rf ../q3/q3/q3_mac ../q3/q3/q3
 rm -f ../q3/q3/image_processor
 rm -f ../q1/q1-mac-executable
-echo -e "\nCleared output_images\n"
+echo -e "\nCleared output_images, and executables from q1 and q3 directories\n"
